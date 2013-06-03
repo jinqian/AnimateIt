dbeffa791a17a03886645b6772b0d2491388d5e4
Qian Jin <qian@moodstocks.com>
2013-06-03 15:04:09 +0200

    version 1
    using translate animation to animate a long horizontal rectangle filled
    up by stripe pattern under the button

--- src/com/example/testproject/MainActivity.java
+++ src/com/example/testproject/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.testproject;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.CycleInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -25,19 +26,22 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	@SuppressLint("NewApi")
 	public void onClickStart(View view) {
 		Button startBtn = (Button) findViewById(R.id.start);
-//		startBtn.setVisibility(View.INVISIBLE);
-		ImageView loadingImage = (ImageView) findViewById(R.id.loading);
+		startBtn.setBackgroundResource(R.drawable.btn_mask_default);
+		final ImageView loadingImage = (ImageView) findViewById(R.id.loading);
 		loadingImage.setVisibility(View.VISIBLE);
 		
-		startBtn.setBackgroundResource(R.drawable.btn_mask_default);
-		Animation loadingAnimation = AnimationUtils.loadAnimation(this, R.anim.anim);
-		loadingImage.startAnimation(loadingAnimation);
-		loadingAnimation.setInterpolator(new CycleInterpolator(0));
-//		AnimationDrawable buttonAnimation = (AnimationDrawable) startBtn.getBackground();
-		
-//		buttonAnimation.start();
+		final Animation loadingAnimation1 = AnimationUtils.loadAnimation(this, R.anim.anim);
+		loadingAnimation1.setInterpolator(new LinearInterpolator());
+		loadingAnimation1.setFillEnabled(true);
+		loadingAnimation1.setFillBefore(true);
+		loadingAnimation1.setFillAfter(true);
+		loadingAnimation1.setStartOffset(10);
+		loadingImage.invalidate();
+		loadingImage.startAnimation(loadingAnimation1);
+
 		startBtn.setText("Loading...");
 	}
 }
